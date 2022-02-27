4a30f9bd85402eb3cad2665797593ce808da618f
Daniel Ribeiro <65352299+academicodan@users.noreply.github.com>
2022-02-27 17:02:14 -0300

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-const lazyHome = React.lazy(() => import('./pages/Home'));
-const lazyClientList = React.lazy(() => import('./pages/ClientList'));
+const lazyHome = React.lazy(() => import("./pages/Home"));
+const lazyClientList = React.lazy(() => import("./pages/ClientList"));
 
-
-const link = from([
-  new HttpLink({uri: "http://localhost:3001/graphql" })
-]);
+const link = from([new HttpLink({ uri: "http://localhost:3001/graphql" })]);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
-})
+  link,
+});
 
 function App() {
   const { t } = useTranslation();
 
-  const loading = t('fallback');
+  const loading = t("fallback");
 
   return (
     <BrowserRouter>
       <Switch>
         <ApolloProvider client={client}>
-          <React.Suspense fallback={loading} >
-            <Route path='/' exact component={lazyHome} />
-            <Route path='/clientes' component={lazyClientList} />
+          <React.Suspense fallback={loading}>
+            <Route path="/" exact component={lazyHome} />
+            <Route path="/clientes" component={lazyClientList} />
           </React.Suspense>
         </ApolloProvider>
       </Switch>
