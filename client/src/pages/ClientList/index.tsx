9a32f79e8d54721395022b43f4163f9c2851b98e
Daniel Ribeiro <65352299+academicodan@users.noreply.github.com>
2022-02-27 17:04:14 -0300

    Add files via upload

--- client/src/pages/ClientList/index.tsx
+++ client/src/pages/ClientList/index.tsx
@@ -2,21 +2,52 @@ import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { Cliente } from "../../types/cliente";
 import { LOAD_CLIENTES } from "../../graphql/Queries";
-
+import { Container } from "../../containers";
+import { Table } from "react-bootstrap";
+import "./styles.scss";
+import { useTranslation } from "react-i18next";
 
 const ClientList = () => {
-    const [clientes, setClientes] = useState<Cliente[]>([])
-    const { data, loading, error} = useQuery(LOAD_CLIENTES);
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+  const { data, loading, error } = useQuery(LOAD_CLIENTES);
 
-    useEffect(() => {
-        console.log(data);
-        if(data) {
-            setClientes(data.getAllClientes);
-        }
-    }, [data])
+  const { t } = useTranslation();
 
+  useEffect(() => {
+    if (data) {
+      setClientes(data.getAllClientes);
+    }
+  }, [data]);
 
-    return <>{JSON.stringify(clientes)}</>
+  return (
+    <Container>
+      <h2 className="title-client">{t('clients.title')}</h2>
+      <p className="description-client">
+        {t('clients.description')}
+      </p>
+      <hr className="solid" />
+      <Table striped bordered hover>
+        <thead
+          style={{
+            backgroundColor: "#14213D",
+            color: "#FFFFFF",
+          }}
+        >
+          <th>{t('clients.firstname')}</th>
+          <th>{t('clients.lastname')}</th>
+          <th>{t('clients.email')}</th>
+        </thead>
+        <tbody>
+          {clientes.map((client, idx) => (
+            <tr key={idx}>
+              <td>{client.nome}</td>
+              <td>{client.sobrenome}</td>
+              <td>{client.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
-
 export default ClientList;
